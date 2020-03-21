42137ac699e5e4b6317c13baa89119c55b2c6549
Robin Aguilar <robin.aguilar@gmail.com>
2020-03-21 17:39:49 -0500

    set up subscription model, started on members/login routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -16,6 +16,16 @@ module.exports = function(app) {
   app.get("/signup", function(req, res) {
     res.render("signup");
   });
+
+  app.get("/members", isAuthenticated, function(req, res){
+    // db.User.findOne({ where: { id: req.params.id } }).then(function(dbUser) {
+    res.render("members");
+    // res.render("members", {
+    //     user: dbUser
+    //   });
+    // });
+  });
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
